67fc4c60c8d410df06cdffe8fde02bb80c83254d
MickeyNeo <20520670@gm.uit.edu.vn>
2022-12-03 03:22:54 +0700

    Xong dc Language

--- Store/action.js
+++ Store/action.js
@@ -1,6 +1,11 @@
 import { SET_HABIT_INPUT } from './constants'
-
+import { SET_LANGUAGE } from './constants'
 export const setHabitInput = payload => ({
     type: SET_HABIT_INPUT,
     payload 
+})
+
+export const setLanguage =payload => ({
+        type: SET_LANGUAGE,
+        payload
 })
--- Store/index.js
+++ Store/index.js
@@ -1,5 +1,8 @@
+import { setLanguage } from './action'
+
 export { default as StoreProvider }  from './Provider'
 export { default as StoreContext } from './Context'
 export * from './hooks'
 
 //export * as actions from './action'
+export {setLanguage} from './action'
