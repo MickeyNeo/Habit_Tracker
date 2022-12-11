6eb8f7a8305e916e2719a2977bab59bbbcf242db
Mrthuancao <mrthuancaogl@gmail.com>
2022-12-11 17:05:04 +0700

    move to addhabit

--- screen/addhabit.js
+++ screen/addhabit.js
@@ -1,4 +1,4 @@
-import React, { useState, Component, useContext,useReducer } from "react";
+import React, { useState, Component, useContext,useReducer, useEffect } from "react";
 import { View, Button, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView,Image, TextInput, Alert, } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
@@ -10,6 +10,10 @@ import themeContext from "./styles/themeContext";
 import ChooseColor from "./icon_color/chooseColor";
 import ChooseIcon from "./icon_color/chooseIcon";
 import Icon from "./icon_color/Icon";
+import * as SQLite from 'expo-sqlite';
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
+
 
 
 import { useStore} from '../Store'
@@ -24,6 +28,106 @@ const AddHabit = ({navigation, route}) => {
     const [isEnabled, setIsEnabled] = useState(false);
     const [changecolor, setcolor] = useState(colors);
     const [text, setText] = useState('');
+    const [db, setDb] = useState(SQLite.openDatabase('C:\Users\ADMIN\OneDrive\Máy tính\HK5\MobileDev\Habit_Tracker\Habit_tracker.db'));
+   const [names, setNames] = useState([]);
+   const [currentName, setCurrentName] = useState(undefined); 
+
+   /* FileSystem.downloadAsync(
+      'C:\Users\ADMIN\OneDrive\Máy tính\HK5\MobileDev\Habit_Tracker\Habit_tracker.db',
+      FileSystem.documentDirectory + 'Habit_tracker.db'
+    )
+    .then(({ uri }) => {
+      console.log('Finished downloading to ', uri)
+    })
+    .catch(error => {
+      console.error(error);
+    }) */
+
+   useEffect(() => {
+
+       db.transaction(tx => {
+         tx.executeSql('CREATE TABLE IF NOT EXISTS "Habit" (\
+            "id"	INTEGER,\
+            "name"	TEXT NOT NULL,\
+            "note"	TEXT,\
+            "frequency"	TEXT NOT NULL,\
+            "color"	TEXT NOT NULL DEFAULT \'#000\',\
+            "tagID"	INTEGER COLLATE BINARY,\
+            "frequencyType"	TEXT NOT NULL CHECK("frequencyType" IN (\'Daily\', \'Weekly\', \'Monthly\')),\
+            "timeRange"	TEXT NOT NULL CHECK("timeRange" IN (\'Anytime\', \'Morning\', \'Afternoon\', \'Evening\')),\
+            "reminderMessage"	TEXT,\
+            "showMemo"	INTEGER NOT NULL CHECK("showMemo" IN (0, 1)),\
+            "chartType"	INTEGER NOT NULL CHECK("chartType" IN (0, 1)),\
+            "habitStartDate"	TEXT NOT NULL,\
+            "habitEndDate"	TEXT,\
+            "goalNo"	INTEGER NOT NULL DEFAULT 1,\
+            "goalPeriod"	TEXT NOT NULL CHECK("goalPeriod" IN (\'Day\', \'Week\', \'Month\')),\
+            "unitID"	INTEGER,\
+            PRIMARY KEY("id" AUTOINCREMENT))')
+       });
+
+       /* db.transaction(tx => {
+         tx.executeSql("INSERT INTO 'Habit' VALUES \
+         ('id', 'name', 'note', 'frequency', 'color', 'tagID', 'frequencyType', 'timeRange', 'reminderMessage', 'showMemo', 'chartType', 'habitStartDate', 'habitEndDate', 'goalNo', 'goalPeriod', 'unitID')\
+            ('1', 'Meditation', 'Meditation is good', '1', '#000', '', 'Daily', 'Morning', 'Now is present!!!', '1', '0', '2022-11-01 00:00:00.000', '', '1', 'Day', '2');\
+            ('2', 'Exercise', 'Exercise is good!!!', '3', '''#002''', '', 'Weekly', 'Anytime', 'Exercise now!!!', '0', '1', '2022-11-01 06:30:00.000', '', '1', 'Day', '8');"
+            )
+       }); */
+
+       db.transaction(tx => {
+         tx.executeSql('CREATE TABLE "Memo" (\
+            "habitID"	INTEGER,\
+            "date"	TEXT,\
+            "content"	TEXT,\
+            PRIMARY KEY("habitID","date"))'
+         )
+      });
+
+      db.transaction(tx => {
+         tx.executeSql('CREATE TABLE "Reminder" (\
+            "habitID"	INTEGER,\
+            "time"	TEXT,\
+            PRIMARY KEY("habitID","time")\
+         )'
+         )
+      });
+
+      db.transaction(tx => {
+         tx.executeSql('CREATE TABLE "Tag" (\
+            "id"	INTEGER,\
+            "name"	TEXT NOT NULL,\
+            PRIMARY KEY("id" AUTOINCREMENT)\
+         )'
+         )
+      });
+
+      db.transaction(tx => {
+         tx.executeSql('CREATE TABLE "Unit" (\
+            "id"	INTEGER,\
+            "name"	TEXT,\
+            PRIMARY KEY("id" AUTOINCREMENT)\
+         )'
+         )
+      }); 
+     
+
+      db.transaction(tx => {
+         tx.executeSql('SELECT * FROM Habit', null,
+         (txObj, resultSet) => {
+            console.log(resultSet.rows._array);
+            if (typeof resultSet.rows._array !== "undefined") {
+              setNames(resultSet.rows._array)
+            }
+          },
+            (txObj, error) => console.log(error)
+         );
+      });
+
+      console.log(names);
+
+   }, [db]);
+
+   console.log(FileSystem.documentDirectory);
     //const onSubmitEditing = (text) => {dispatch(setHabitInput(text))}
     //console.log(text);
     console.log(state.name);
