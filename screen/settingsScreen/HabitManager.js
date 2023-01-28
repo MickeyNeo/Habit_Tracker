233b65669701a4764cc03368f2052fc899cb38ef
MickeyNeo <20520670@gm.uit.edu.vn>
2023-01-29 01:21:12 +0700

    1 nua Tag

--- screen/settingsScreen/HabitManager.js
+++ screen/settingsScreen/HabitManager.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { useState } from 'react';
-import {Text,  View, StyleSheet,ScrollView,TouchableOpacity } from 'react-native';
+import {Text,  View, StyleSheet,ScrollView,TouchableOpacity,SafeAreaView,TouchableWithoutFeedback } from 'react-native';
 import { useStore } from "../../Store";
 import { FontAwesome5,MaterialCommunityIcons } from "@expo/vector-icons";
+import Modal from "react-native-modal";
+import SelectDropdown from 'react-native-select-dropdown'
 export default function HabitManager (){
     const[state, dispatch] =useStore()
     const [test, setList] =useState(['Action 1',' Action 2', 'Action 3'])
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [popUpPosition, setPopUpPosition] = useState({x:0, y:0});
+    const onPress = (event) => {
+        const {pageX, pageY} = event.nativeEvent;
+        setPopUpPosition({x:pageX, y:pageY});
+        console.log(`Tapped at x: ${pageX}, y: ${pageY}`);
+    }
+    const option = ["Edit", "Reset All Data", "Delete"]
     return(         
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
            <ScrollView style={styles.scroll} >
                     {test.map((value,index) =>( 
                         <View key={index}>
@@ -21,9 +32,41 @@ export default function HabitManager (){
                                                 loi nhac
                                             </Text>
                                         </View>
-                                        <TouchableOpacity >
-                                            <MaterialCommunityIcons style={styles.iconDots} name="dots-horizontal-circle-outline" size={24} color="black" />
-                                        </TouchableOpacity>
+                                        <TouchableWithoutFeedback onPress={()=> {setModalVisible(true) }}>
+                                            <View style={styles.iconDots}>
+
+                                                <SelectDropdown
+                                                    data={option}
+                                                    // defaultButtonText={<View style={{AlignItems:'center'}}>
+                                                    //         <MaterialCommunityIcons  name="dots-horizontal-circle-outline" size={20} color="black" />
+                                                    // </View>
+                                                    // }
+                                                    defaultButtonText={<Text style={{alignSelf:'flex-start'}}>...</Text>}
+                                                    buttonStyle={{
+                                                        width: 50,
+                                                        height: 50,
+                                                        borderRadius: 25,
+                                                        backgroundColor: 'gray'}}
+                                                    //style={{ width: 100, height: 100}}
+                                                    dropdownStyle={{
+                                                        marginLeft:'-13%',
+                                                        width: 180, height: 150, backgroundColor: 'white'
+                                                    }}
+                                                    onSelect={(selectedItem, index) => 
+                                                        {
+                                                          if (selectedItem == option[0]) console.log(selectedItem)
+                                                          else if (selectedItem == option[1]) console.log(selectedItem)
+                                                          else console.log(selectedItem)
+                                                        }}
+                                                    
+                
+                                                />
+                                               
+                                            </View>
+                                            
+                                                
+                                            
+                                        </TouchableWithoutFeedback>
                             </View>
                             
                        
@@ -31,7 +74,7 @@ export default function HabitManager (){
                        
                     ))}
                 </ScrollView>  
-        </View>
+        </SafeAreaView>
     );
 
 }
@@ -39,6 +82,7 @@ const styles=StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:'white',
+        
     },
     scroll:{
         //marginLeft: 5,
@@ -47,7 +91,8 @@ const styles=StyleSheet.create({
     iconTitle:{
         flexDirection:'row', 
         marginLeft: 20,
-        marginTop: 50
+        marginTop: 50,
+        
     },
     viewText:{
         flex:1,
@@ -62,8 +107,32 @@ const styles=StyleSheet.create({
         fontWeight: '100'
     },
     iconDots:{
-        marginTop: 15,
+        marginTop: '1%',
         AlignItems: 'flex-end',
-        marginRight:"8%"
-    }
+        marginRight:"9%",
+        
+
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
 })
