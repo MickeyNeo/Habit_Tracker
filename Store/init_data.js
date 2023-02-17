f75f39cd2b598eb88db4f0f049480224dafb4f0f
Mrthuancao <mrthuancaogl@gmail.com>
2023-02-17 09:20:14 +0700

    fix habit data more

--- Store/init_data.js
+++ Store/init_data.js
@@ -1,11 +1,11 @@
 export const habitInit = "INSERT INTO Habit (name, note, frequency, color, frequencyType, timeRange,\
     reminderMessage, showMemo, chartType, habitStartDate, habitEndDate, goalNo, goalPeriod, unitID, icon, iconFamily, id) VALUES\
-    ('Dancing', 'Nothing to note', 'MON,TUE,THU', '#FFAEAE', 'Week', 'Evening', 'Remember to dance', '1', '0', '2022-06-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
+    ('Dancing', 'Nothing to note', 'MON,TUE,THU', '#FFAEAE', 'Weekly', 'Evening', 'Remember to dance', '1', '0', '2022-06-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
     '30', 'Week', '2', 'walking', 'FontAwesome5','dc'),\
-    ('Meditate', 'Nothing to note', 'Daily', '#FFAEAE', 'Day', 'Morning', 'Remember to meditate', '0', '0', '2021-02-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
+    ('Meditate', 'Nothing to note', 'Daily', '#FFAEAE', 'Daily', 'Morning', 'Remember to meditate', '0', '0', '2021-02-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
     '10', 'Day', '2', 'walking', 'FontAwesome5','md'),\
-    ('Run', 'Nothing to note', '6, 7, 13, 14, 20, 21', '#FFAEAE', 'Week', 'Morning', 'Remember to run', '1', '0', '2022-02-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
-    '2', 'Week', '9', 'walking', 'FontAwesome5','rn')"
+    ('Run', 'Nothing to note', '6, 7, 13, 14, 20, 21', '#FFAEAE', 'Monthly', 'Morning', 'Remember to run', '1', '0', '2022-02-15T14:46:07.830Z', '2023-02-15T14:46:07.830Z',\
+    '2', 'Day', '9', 'walking', 'FontAwesome5','rn')"
 
 export const memoInit = "INSERT INTO Memo (habitName, date, content, progress) VALUES\
 ('Run', '2023:02:21', 'Im on a roll', '0'),\
